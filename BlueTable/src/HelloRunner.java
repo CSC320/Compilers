c5bd81adb97332763f67ad5d9fd2f03eeae79c55
garryd635 <garryd635@gmail.com>
2017-04-19 19:10:29 -0400

    Pushing hellorunner with error checking

--- BlueTable/src/HelloRunner.java
+++ BlueTable/src/HelloRunner.java
@@ -13,25 +13,32 @@ import org.antlr.v4.runtime.*;
 
             ANTLRFileStream input = new ANTLRFileStream("oj_2.txt");
             System.out.print(input);
-            HelloLexer lexer = new HelloLexer(input);
+            GrammarCSVLexer lexer = new GrammarCSVLexer(input);
 
             CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-            HelloParser parser = new HelloParser(tokens);
+            GrammarCSVParser parser = new GrammarCSVParser(tokens);
             ParseTree tree = parser.r(); // begin parsing at rule 'r'
             System.out.println(tree.toStringTree(parser)); // print LISP-style tree
-            
-          //show AST in GUI
-            JFrame frame = new JFrame("Antlr AST");
-            JPanel panel = new JPanel();
-            TreeViewer viewr = new TreeViewer(Arrays.asList(
-                    parser.getRuleNames()),tree);
-            viewr.setScale(1.5);//scale a little
-            panel.add(viewr);
-            frame.add(panel);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(200,200);
-            frame.setVisible(true);
-            
+            int error = parser.getNumberOfSyntaxErrors();
+            if(error > 0){
+            	System.out.println("Error Occurred");
+            }else{
+            	System.out.println("Nice!");
+            	System.out.println(input.toString());
+				Antler_Command_Parser commandParser = new Antler_Command_Parser(input.toString()); 
+            	//show AST in GUI
+                JFrame frame = new JFrame("Antlr AST");
+                JPanel panel = new JPanel();
+                TreeViewer viewr = new TreeViewer(Arrays.asList(
+                        parser.getRuleNames()),tree);
+                viewr.setScale(1.5);//scale a little
+                panel.add(viewr);
+                frame.add(panel);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(200,200);
+                frame.setVisible(true);
+            }
+                  
         }
 }
